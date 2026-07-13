646f6c91cd5b44d55f7755940d449d4c325173bb
rpal68 <praxislutz@googlemail.com>
2026-07-13 21:36:23 +0200

    Add files via upload

--- urlaub.js
+++ urlaub.js
@@ -9,7 +9,11 @@
 window.HLZ_URLAUB = {
   vorlauf_tage: 14,   // Vorab-Hinweis erscheint so viele Tage vorher
   zeitraeume: [
-    { von: "2026-09-21", bis: "2026-10-02", vertretung: "" },
-    // { von: "2026-12-24", bis: "2027-01-01", vertretung: "Praxis Dr. Mustermann, Tel. 09721 12345" },
+    { von: "2026-08-03", bis: "2026-08-07",
+      vertretung: "Dr. Wagner (Tel. 09721 23511), Dr. Nied, Niederwerrn (Tel. 09721 9418000) und Dr. Konz (Tel. 09721 84182)" },
+    { von: "2026-09-21", bis: "2026-10-02",
+      vertretung: "Dr. Wagner (Tel. 09721 23511) und Dr. Nied, Niederwerrn (Tel. 09721 9418000)" },
+    { von: "2026-12-24", bis: "2027-01-06",
+      vertretung: "bis 03.01. Dr. Wagner (Tel. 09721 23511) und Dr. Prunkl (Tel. 09721 44454), ab 04.01. Dr. Bashti (Tel. 09721 26222)" },
   ]
 };
