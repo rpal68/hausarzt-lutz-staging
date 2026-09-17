d34a8055e4b10cc8d1dc51ffff0ed367d0a4c91d
rpal68 <praxislutz@googlemail.com>
2026-09-17 20:32:27 +0200

    Update urlaub.js

--- urlaub.js
+++ urlaub.js
@@ -12,7 +12,7 @@ window.HLZ_URLAUB = {
     { von: "2026-08-03", bis: "2026-08-07",
       vertretung: "Dr. Wagner (Tel. 09721 23511), Dr. Nied, Niederwerrn (Tel. 09721 9418000) und Dr. Konz (Tel. 09721 84182)" },
     { von: "2026-09-21", bis: "2026-10-02",
-      vertretung: "Dr. NN (Tel. 09721 xxxx) und Dr. Nied, Niederwerrn (Tel. 09721 9418000)" },
+      vertretung: "Dr. Michael Hofmann (Tel. 09721 21089 ) und Dr. Nied, Niederwerrn (Tel. 09721 9418000)" },
     { von: "2026-12-24", bis: "2027-01-06",
       vertretung: "bis 03.01. Dr. Wagner (Tel. 09721 23511) und Dr. Prunkl (Tel. 09721 44454), ab 04.01. Dr. Bashti (Tel. 09721 26222)" },
   ]
